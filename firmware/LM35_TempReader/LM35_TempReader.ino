d552a6d8eec34db95833def28436c739e98cad96
Nero <yng.neero@gmail.com>
2026-04-15 15:40:59 +0700

    feat: ho tro doc 2 kenh LM35 (A0, A1)

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,11 +1,19 @@
+const int pins[] = {A0, A1}; // Mảng chứa các chân cảm biến
+int adcValues[2];            // Mảng lưu giá trị ADC
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("Time,Sensor_A0,Sensor_A1");
 }
 
 void loop() {
-  int adcValue = analogRead(A0);
-  float tempC = (adcValue * 500.0) / 1023.0;
-  Serial.print("Nhiet do: ");
-  Serial.println(tempC);
+  Serial.print(millis());
+  for (int i = 0; i < 2; i++) {
+    adcValues[i] = analogRead(pins[i]);
+    float temp = (adcValues[i] * 500.0) / 1023.0;
+    Serial.print(",");
+    Serial.print(temp);
+  }
+  Serial.println();
   delay(1000);
 }
