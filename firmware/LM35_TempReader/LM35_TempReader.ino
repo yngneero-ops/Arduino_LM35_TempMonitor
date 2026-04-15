95b00a2e71e621d3b71135cb586dbe3f574c66fa
Nguyen Phan Tan Dat <clirt@example.com>
2026-04-15 18:16:56 +0700

    feat: doi dinh dang Serial sang JSON

--- firmware/LM35_TempReader/LM35_TempReader.ino
+++ firmware/LM35_TempReader/LM35_TempReader.ino
@@ -1,19 +1,15 @@
-const int pins[] = {A0, A1}; // Mảng chứa các chân cảm biến
-int adcValues[2];            // Mảng lưu giá trị ADC
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Time,Sensor_A0,Sensor_A1");
-}
-
 void loop() {
-  Serial.print(millis());
-  for (int i = 0; i < 2; i++) {
-    adcValues[i] = analogRead(pins[i]);
-    float temp = (adcValues[i] * 500.0) / 1023.0;
-    Serial.print(",");
-    Serial.print(temp);
-  }
-  Serial.println();
+  // SV B vẫn đọc 2 kênh như cũ
+  adcValues[0] = analogRead(A0);
+  nhietDo[0] = (adcValues[0] * 500.0) / 1023.0;
+  adcValues[1] = analogRead(A1);
+  nhietDo[1] = (adcValues[1] * 500.0) / 1023.0;
+
+  // SV B sửa định dạng xuất JSON 
+  Serial.print("{");
+  Serial.print("\"t1\": "); Serial.print(nhietDo[0], 1);
+  Serial.print(", \"t2\": "); Serial.print(nhietDo[1], 1);
+  Serial.println("}");
+
   delay(1000);
 }
