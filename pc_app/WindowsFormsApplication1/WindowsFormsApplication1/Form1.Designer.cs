d3fa93daedbb77941d60a2c3d6e55a4321b0622d
Nero <yng.neero@gmail.com>
2026-04-15 17:06:17 +0700

    feat: them ung dung C# hien thi do thi nhiet do

---
+++ pc_app/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbxTenCom = new System.Windows.Forms.ComboBox();
+            this.btnKetNoi = new System.Windows.Forms.Button();
+            this.UART = new System.IO.Ports.SerialPort(this.components);
+            this.btnNgatKetNoi = new System.Windows.Forms.Button();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.miCompact = new System.Windows.Forms.ToolStripMenuItem();
+            this.miScroll = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tên Com";
+            // 
+            // cbxTenCom
+            // 
+            this.cbxTenCom.FormattingEnabled = true;
+            this.cbxTenCom.Location = new System.Drawing.Point(96, 20);
+            this.cbxTenCom.Name = "cbxTenCom";
+            this.cbxTenCom.Size = new System.Drawing.Size(175, 27);
+            this.cbxTenCom.TabIndex = 1;
+            // 
+            // btnKetNoi
+            // 
+            this.btnKetNoi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnKetNoi.Location = new System.Drawing.Point(31, 65);
+            this.btnKetNoi.Name = "btnKetNoi";
+            this.btnKetNoi.Size = new System.Drawing.Size(123, 39);
+            this.btnKetNoi.TabIndex = 2;
+            this.btnKetNoi.Text = "Kết Nối";
+            this.btnKetNoi.UseVisualStyleBackColor = true;
+            this.btnKetNoi.Click += new System.EventHandler(this.btnKetNoi_Click);
+            // 
+            // UART
+            // 
+            this.UART.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.UART_DataReceived);
+            // 
+            // btnNgatKetNoi
+            // 
+            this.btnNgatKetNoi.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNgatKetNoi.Enabled = false;
+            this.btnNgatKetNoi.Location = new System.Drawing.Point(160, 65);
+            this.btnNgatKetNoi.Name = "btnNgatKetNoi";
+            this.btnNgatKetNoi.Size = new System.Drawing.Size(123, 39);
+            this.btnNgatKetNoi.TabIndex = 3;
+            this.btnNgatKetNoi.Text = "Ngắt Kết Nối";
+            this.btnNgatKetNoi.UseVisualStyleBackColor = true;
+            this.btnNgatKetNoi.Click += new System.EventHandler(this.btnNgatKetNoi_Click);
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Location = new System.Drawing.Point(290, 23);
+            this.zedGraphControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(716, 378);
+            this.zedGraphControl1.TabIndex = 4;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.miCompact,
+            this.miScroll});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(124, 48);
+            // 
+            // miCompact
+            // 
+            this.miCompact.Name = "miCompact";
+            this.miCompact.Size = new System.Drawing.Size(152, 22);
+            this.miCompact.Text = "Compact";
+            this.miCompact.Click += new System.EventHandler(this.miCompact_Click);
+            // 
+            // miScroll
+            // 
+            this.miScroll.Name = "miScroll";
+            this.miScroll.Size = new System.Drawing.Size(152, 22);
+            this.miScroll.Text = "Scroll";
+            this.miScroll.Click += new System.EventHandler(this.miScroll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 414);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.btnNgatKetNoi);
+            this.Controls.Add(this.btnKetNoi);
+            this.Controls.Add(this.cbxTenCom);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbxTenCom;
+        private System.Windows.Forms.Button btnKetNoi;
+        private System.IO.Ports.SerialPort UART;
+        private System.Windows.Forms.Button btnNgatKetNoi;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem miCompact;
+        private System.Windows.Forms.ToolStripMenuItem miScroll;
+    }
+}
+
---
+++ pc_app/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using ZedGraph;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        #region quan ly bien
+        long tickStart = 0;
+        public enum _enCheDo { Compact = 0, Scroll };
+        _enCheDo CheDo = _enCheDo.Compact;
+
+        #endregion
+
+        #region quan ly ham
+        private void _KhoiTao()
+        {
+            cbxTenCom.DataSource = SerialPort.GetPortNames();
+
+            for (int i = 0; i < cbxTenCom.Items.Count; i++)
+            {
+                if (cbxTenCom.Items[i].ToString() == "COM2")
+                {
+                    cbxTenCom.Text = "COM2";
+                    break;
+                }
+            }
+        }
+
+        public void draw(double setpoint1, double setpoint2)
+        {
+            //make sure that curvelist has at least one curve
+            if (zedGraphControl1.GraphPane.CurveList.Count <= 0)
+                return; // Kiểm tra việc khởi tạo các đường curve
+
+            LineItem curve1 = zedGraphControl1.GraphPane.CurveList[0] as LineItem;
+            LineItem curve2 = zedGraphControl1.GraphPane.CurveList[1] as LineItem;
+            if (curve1 == null)
+                return;
+            if (curve2 == null)
+                return;
+
+            IPointListEdit list1 = curve1.Points as IPointListEdit;
+            IPointListEdit list2 = curve2.Points as IPointListEdit;
+            if (list1 == null)
+                return;
+            if (list2 == null)
+                return;
+
+            tickStart = tickStart + 1;
+            double time = tickStart;
+            //time = (Environment.TickCount - tickStart) / 500.0;
+
+            list1.Add(time, setpoint1);// Đây chính là hàm hiển thị dữ liệu của mình lên đồ thị
+            list2.Add(time, setpoint2);
+
+            //keep the X scale at a rolling 30 seconed interval, with one major step
+            //between the max X value and the end of the axis
+            Scale xScale = zedGraphControl1.GraphPane.XAxis.Scale;
+            if (time > xScale.Max - xScale.MajorStep)
+            {
+                if (CheDo == _enCheDo.Compact)
+                {
+                    xScale.Max = time + xScale.MajorStep;
+                    xScale.Min = xScale.Max - 30.0;
+                }
+                else
+                {
+                    xScale.Max = time + xScale.MajorStep;
+                    xScale.Min = 0;
+                }
+            }
+
+            //make sure the y axis is rescaled to accommodate actual data
+            zedGraphControl1.AxisChange();
+            // Force a redraw
+            zedGraphControl1.Invalidate();
+        }
+        #endregion
+
+        #region quan ly form
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+
+            _KhoiTao();
+
+            GraphPane myPane = zedGraphControl1.GraphPane;
+            myPane.Title.Text = "ĐỒ THỊ NHIỆT ĐỘ TỪ CẢM BIẾN LM35";
+            myPane.XAxis.Title.Text = "Thời Gian";
+            myPane.YAxis.Title.Text = "Nhiệt Độ";
+            RollingPointPairList list1 = new RollingPointPairList(2000);
+            RollingPointPairList list2 = new RollingPointPairList(2000);
+            LineItem curve1 = myPane.AddCurve("Nhiệt Độ 1", list1, Color.Blue, SymbolType.None);
+            LineItem curve2 = myPane.AddCurve("Nhiệt Độ 2", list2, Color.Black, SymbolType.None);
+            myPane.XAxis.Scale.Min = 0;  // Min = 0;
+            myPane.XAxis.Scale.Max = 30; // Max = 30;
+            myPane.XAxis.Scale.MinorStep = 1;  // Đơn vị chia nhỏ nhất 1
+            myPane.XAxis.Scale.MajorStep = 5; // Đơn vị chia lớn 5
+
+            //scale the axes
+            zedGraphControl1.AxisChange();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Escape:
+                    {
+                        Close();
+                        break;
+                    }
+            }
+        }
+        #endregion
+
+        string Temp = "";
+        int[] NhietDo = new int[2] { 0,0};
+        private void UART_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            Temp = UART.ReadTo("\n");
+            var Tach = Temp.Split(',');
+
+            if (Tach.Length > 1)
+            {
+                NhietDo[0] = Convert.ToInt32(Tach[0]);
+                NhietDo[1] = Convert.ToInt32(Tach[1]);
+            }
+
+
+            draw(NhietDo[0], NhietDo[1]);
+
+        }
+
+        private void btnKetNoi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UART.BaudRate = 9600;
+                UART.PortName = cbxTenCom.Text;
+                UART.Open();
+                if (UART.IsOpen == true)
+                {
+                    btnKetNoi.Enabled = false;
+                    btnNgatKetNoi.Enabled = true;
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);                
+            }
+        }
+
+        private void btnNgatKetNoi_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                UART.Close();
+                if (UART.IsOpen == false)
+                {
+                    btnKetNoi.Enabled = true;
+                    btnNgatKetNoi.Enabled = false;
+
+                }
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+        }
+
+        private void miCompact_Click(object sender, EventArgs e)
+        {
+            CheDo = _enCheDo.Compact;
+        }
+
+        private void miScroll_Click(object sender, EventArgs e)
+        {
+            CheDo = _enCheDo.Scroll;
+        }
+    }
+}
